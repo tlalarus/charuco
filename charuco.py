c536daf4843cd22f0cbf871d8036b77fd18e26ea
minkyung <sim_mmmmk@golfzon.com>
2025-06-27 16:35:13 +0900

    Initial commit

---
+++ charuco.py
@@ -0,0 +1,114 @@
+import cv2
+import numpy as np
+
+
+print('opencv version: ', cv2.__version__) 
+print()
+
+dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
+
+# 입력값
+dpi = 300
+
+width_mm = 290
+height_mm = 200
+
+square_x_count = 11
+square_y_count = 8
+square_length_mm = 20
+marker_length_mm = 15
+
+margin_minimum_mm = 0 # 양쪽 마진 최소값 
+
+display_scale = 0.2
+
+remain_width = width_mm - (square_x_count * square_length_mm)
+remain_height = height_mm - (square_y_count * square_length_mm)
+print('remain_width_mm: ', remain_width)
+print('remain_height_mm: ', remain_height)
+print()
+
+# margin_mm = min([remain_width, remain_height])
+margin_mm = 0
+print('margin_mm: ', margin_mm)
+
+### Verify
+width_total_mm = square_x_count * square_length_mm + (margin_mm)
+height_total_mm = square_y_count * square_length_mm + (margin_mm)
+
+
+print('width_total_mm: ', width_total_mm)
+print('height_total_mm: ', height_total_mm)
+
+if(width_total_mm > width_mm):
+  print('width is too long !!!')
+  quit()
+
+if(height_total_mm > height_mm):
+  print('height is too long !!!')
+  quit()
+
+if(margin_mm < margin_minimum_mm):
+  print('margin is too short !!!')
+  quit()
+
+### Calculate
+width_inch = width_mm / 25.4
+height_inch = height_mm / 25.4
+
+width_px = int(width_inch * dpi)
+height_px = int(height_inch * dpi)
+
+margin_px = int(margin_mm / 25.4 * dpi / 2)
+
+width_without_margin_px = int(width_total_mm / 25.4 * dpi)
+height_without_margin_px = int(height_total_mm / 25.4 * dpi)
+
+
+print()
+print('resolution px: ', width_px, height_px)
+print('margin px: ', margin_px)
+
+
+board = cv2.aruco.CharucoBoard(
+  size=(square_x_count, square_y_count),
+  squareLength=square_length_mm * 0.001,
+  markerLength=marker_length_mm * 0.001,
+  dictionary=dictionary
+)
+
+  # outSize=(9449, 7087),
+img = board.generateImage(
+  outSize=(width_without_margin_px, height_without_margin_px),
+  borderBits=1,
+  marginSize=margin_px
+)
+
+bg_width_mm = 297
+bg_height_mm = 210
+bg_width_px = int(bg_width_mm / 25.4 * dpi)
+bg_height_px = int(bg_height_mm / 25.4 * dpi)
+background = np.ones((bg_height_px, bg_width_px, 3), dtype=np.uint8) * 255
+
+print('background resolution: ', bg_width_px, bg_height_px)
+
+# calculate center position
+top = (bg_height_px - height_without_margin_px) // 2
+left = (bg_width_px - width_without_margin_px) // 2
+print('background insert position: ', top, left)
+
+bgr = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+background[top:top + height_without_margin_px, left:left + width_without_margin_px] = bgr
+
+new_size = (int(width_px*display_scale), int(height_px*display_scale))
+img_display = cv2.resize(img, new_size, interpolation=cv2.INTER_AREA)
+
+background_display = cv2.resize(background, new_size, interpolation=cv2.INTER_AREA)
+
+# cv2.imshow("calibration target", img_display)
+cv2.imshow("calibration target", background_display)
+cv2.waitKey(0)
+
+cv2.imwrite('charuco.png', background)
+
+# 2.7, 1.4
